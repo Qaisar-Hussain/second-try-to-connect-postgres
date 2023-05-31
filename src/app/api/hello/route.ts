d8c062052c8450f7d71ccbc3fdf923cb33f036eb
qaisarhussain <qhussain960@gmail.com>
2023-05-31 23:24:24 +0500

    issue resolved for api get request

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,20 +1,22 @@
 import { db } from '@vercel/postgres';
 import { NextApiRequest, NextApiResponse } from 'next';
- 
-export default async function handler(
+import {NextResponse} from "next/server" 
+export async function GET(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
   const client = await db.connect();
  
   try {
-    await client.sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+    await client.sql`CREATE TABLE Petsss ( Name varchar(255), Owner varchar(255) );`;
     const names = ['Fiona', 'Lucy'];
-    await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
+    await client.sql`INSERT INTO Petsss (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
   } catch (error) {
-    return response.status(500).json({ error });
+    return NextResponse.json({ error });
+    // return response.status(500).json({ error });
   }
  
-  const pets = await client.sql`SELECT * FROM Pets;`;
-  return response.status(200).json({ pets });
+  const pets = await client.sql`SELECT * FROM Petsss;`;
+  return NextResponse.json({ pets });
+  // return response.status(200).json({ pets });
 }
